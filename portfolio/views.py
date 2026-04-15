a7a3d52b11bb57af6499777cafeb933502d3bfdf
Keven Lucas <keven.lucas00@hotmail.com>
2026-04-15 06:57:57 -0300

    novo layou + correção sugestao

--- portfolio/views.py
+++ portfolio/views.py
@@ -25,23 +25,25 @@ def contato(request):
     return render(request, "contato.html")
 
 
+@csrf_exempt
 def enviar_sugestao(request):
     if request.method == 'POST':
-        
-        data = json.loads(request.body)
+        try:
+            data = json.loads(request.body)
 
-        email_usuario = data.get('email')
-        mensagem = data.get('mensagem')
+            email_usuario = data.get('email')
+            mensagem = data.get('mensagem')
 
+            send_mail(
+                subject='📩 Nova sugestão do site',
+                message=f'Email do usuário: {email_usuario}\n\nSugestão:\n{mensagem}',
+                from_email=settings.EMAIL_HOST_USER,
+                recipient_list=[settings.EMAIL_HOST_USER],
+            )
 
-        send_mail(
-            subject='📩 Nova sugestão do site',
-            message=f'Email do usuário: {email_usuario}\n\nSugestão:\n{mensagem}',
-            from_email=settings.EMAIL_HOST_USER,
-            recipient_list=[settings.EMAIL_HOST_USER],
-        )
-        
+            return JsonResponse({'status': 'ok'})
 
-        return JsonResponse({'status': 'ok'})
+        except Exception as e:
+            return JsonResponse({'erro': str(e)}, status=500)
 
     return JsonResponse({'status': 'erro'})
