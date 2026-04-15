6bba9cb23ce64453d5370ce3561d23d938fbdd1f
unknown <keven.lucas00@hotmail.com>
2026-04-15 14:01:01 -0300

    nova correcao email

--- config/settings.py
+++ config/settings.py
@@ -10,7 +10,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_TIMETOU = 10
+EMAIL_TIMEOUT = 10
 
 EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
