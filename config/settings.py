4426046cbb2deada313d4b177eabec9306121e7f
Keven Lucas <keven.lucas00@hotmail.com>
2026-05-07 08:52:46 -0300

    novas configs do email

--- config/settings.py
+++ config/settings.py
@@ -3,24 +3,36 @@ import os
 from typing import cast
 import dj_database_url
 from dotenv import load_dotenv
-load_dotenv()
-
-
-
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 465
-EMAIL_USE_TLS = False
-EMAIL_USE_SSL = True
-EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
-EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
-
-DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+import ssl
 
 BASE_DIR = Path(__file__).resolve().parent.parent
+load_dotenv()
+
+DEBUG = os.environ.get("DEBUG", "True") == "True" 
+
+if DEBUG:
+    EMAIL_BACKEND = "portfolio.email_backend.CustomEmailBackend"
+else:
+    EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+    
+EMAIL_HOST = "smtp.sendgrid.net"
+
+
+EMAIL_PORT = 587
+
+EMAIL_USE_SSL = False
+EMAIL_USE_TLS = True
+
+EMAIL_HOST_USER = "apikey"
+
+EMAIL_HOST_PASSWORD = os.getenv("SENDGRID_API_KEY")
+SENDGRID_API_KEY = os.getenv("SENDGRID_API_KEY")
+
+DEFAULT_FROM_EMAIL = "meusitepessoal2027@outlook.com"
+
 
 SECRET_KEY = os.environ.get("SECRET_KEY", "django-insecure-dev-key")
-DEBUG = os.environ.get("DEBUG", "True") == "True" 
+
 
 ALLOWED_HOSTS = [host.strip() for host in os.environ.get(
     "ALLOWED_HOSTS",
--- portfolio/views.py
+++ portfolio/views.py
@@ -39,13 +39,15 @@ def enviar_sugestao(request):
             email_usuario = data.get('email')
             mensagem = data.get('mensagem')
             
-            print("EMAIL:", settings.EMAIL_HOST_USER)
+            print("EMAIL:", settings.DEFAULT_FROM_EMAIL)
             print("PASS:", settings.EMAIL_HOST_PASSWORD)
 
             # 1. Configuração do Assunto e Destinatários
             subject = f'📩 Nova Sugestão: {assunto_user}'
-            from_email = settings.EMAIL_HOST_USER
-            to = [settings.EMAIL_HOST_USER]
+            to = ["kevenmathers@gmail.com"]
+            
+            from_email = settings.DEFAULT_FROM_EMAIL
+            
 
             # 2. O corpo do e-mail em HTML com CSS Inline (melhor compatibilidade)
             html_content = f"""
