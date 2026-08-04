98bf6a657bd9a29b5aa15be511caabd88f9c6394
Keven Lucas <keven.lucas00@hotmail.com>
2026-08-04 19:33:35 -0300

    nova alteração

--- create_superuser.py
+++ create_superuser.py
@@ -8,6 +8,7 @@ from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
+
 username = os.environ.get("DJANGO_SUPERUSER_USERNAME")
 email = os.environ.get("DJANGO_SUPERUSER_EMAIL")
 password = os.environ.get("DJANGO_SUPERUSER_PASSWORD")
